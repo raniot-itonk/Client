32ad550c7b99383f14d51280847b82cd62c907a5
Anders Meidahl <Andersmeidahl@gmail.com>
2019-03-27 22:46:11 +0100

    Added serilog

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace Client
 {
@@ -7,11 +10,45 @@ namespace Client
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = GetConfiguration(env);
+
+            SetupLogger(env, configuration);
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSerilog();
+
+        private static void SetupLogger(string env, IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Environment", env)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            Log.Information("Logger setup");
+        }
+
+        private static IConfigurationRoot GetConfiguration(string env)
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env}.json")
+                .Build();
+        }
     }
 }
